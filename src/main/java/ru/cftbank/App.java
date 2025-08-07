d0dace1b112c5888a9266ac25ecf76aba135fa35
Gubaev Ruslan <gub_ruslan@mail.ru>
2025-08-07 22:11:48 +0500

    delete output args

--- src/main/java/ru/cftbank/App.java
+++ src/main/java/ru/cftbank/App.java
@@ -16,7 +16,6 @@ public class App {
 
     private static ParameterController parameterController;
     public static void main(String[] args){
-        System.out.println(Arrays.toString(args));
         parameterController = new ParameterController(args);
         parameterController.outputInfo();
 
