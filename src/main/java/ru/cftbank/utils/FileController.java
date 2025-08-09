0af9eba660d61c284b45192a2405c0876e303a2f
Gubaev Ruslan <gub_ruslan@mail.ru>
2025-08-09 06:08:55 +0500

    change file controller

--- src/main/java/ru/cftbank/utils/FileController.java
+++ src/main/java/ru/cftbank/utils/FileController.java
@@ -3,53 +3,51 @@ package ru.cftbank.utils;
 import ru.cftbank.WriteType;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FileController {
+
+    private FileController(){
+    }
+
     public static List<String> getStringArray(String filePath) throws IOException {
-        ArrayList<String> stringList = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        List<String> stringList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(filePath), StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null) {
+                if(!line.isEmpty()){
                 stringList.add(line);
+                }
             }
         }
         return stringList;
     }
 
     public static void setStringArray(Path filePath, List<String> stringList, WriteType writeType) throws IOException {
+        Objects.requireNonNull(writeType, "Тип записи не может быть null");
 
         Path parentDir = filePath.getParent();
-
-        if (parentDir != null && !Files.exists(parentDir)) {
+        if (parentDir != null) {
             Files.createDirectories(parentDir);
         }
 
-        if (Files.exists(filePath) && !Files.isWritable(filePath)) {
-            throw new IOException("Ошибка! Нет прав на запись в файл: " + filePath + ".");
-        }
-
-        FileWriter fileWriter;
-        switch (writeType) {
-            case APPEND:
-                fileWriter = new FileWriter(filePath.toFile(), true);
-                break;
-            case REWRITE:
-                fileWriter = new FileWriter(filePath.toFile(),false);
-                break;
-            default:
-                throw new IOException("Ошибка! Неверный тип записи данных.");
-        }
-
-        try(BufferedWriter writer = new BufferedWriter(fileWriter)) {
-            for(String s : stringList){
+        try (BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(filePath.toFile(), writeType == WriteType.APPEND),
+                        StandardCharsets.UTF_8))) {
+            for (String s : stringList) {
                 writer.write(s);
                 writer.newLine();
             }
+        } catch (IOException e) {
+            throw new IOException("Ошибка записи в файл: " + filePath, e);
         }
-
     }
 }
